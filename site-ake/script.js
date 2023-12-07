5b35a8c8e215f9b4f3f969fec166c5f8d0877259
cicero <139743005+Cisenai@users.noreply.github.com>
2023-12-07 08:02:49 -0300

    Add files via upload

---
+++ site-ake/script.js
@@ -0,0 +1,9 @@
+function mostrarDescricao(livro) {
+    const descricao = livro.querySelector('.descricao');
+    descricao.style.display = 'block';
+}
+
+function ocultarDescricao(livro) {
+    const descricao = livro.querySelector('.descricao');
+    descricao.style.display = 'none';
+}
